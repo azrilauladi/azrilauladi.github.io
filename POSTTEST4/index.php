597d2e0b2f46ed0b8d62204132bded2527202d8e
azrilauladi <148922941+azrilauladi@users.noreply.github.com>
2024-09-29 11:34:36 +0800

    Add files via upload

---
+++ POSTTEST4/index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Rental Mobil</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo"><img src="img/jeep.png" alt=""></a>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#ride">Ride</a></li>
+            <li><a href="#Services">Services</a></li>
+            <li><a href="bio/portfolio.html" id="about-link" target="_blank">About</a></li>
+            <li><a href="#reviews">Reviews</a></li>
+        </ul>
+        <div class="header-btn">
+            <div id="bulanmatahari">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 36 36"><path fill="#fe5b3d" d="M18 6.31a1 1 0 0 0 1-1v-3.4a1 1 0 0 0-2 0v3.4a1 1 0 0 0 1 1" class="clr-i-outline clr-i-outline-path-1"/><path fill="#fe5b3d" d="M18 29.69a1 1 0 0 0-1 1v3.4a1 1 0 0 0 2 0v-3.4a1 1 0 0 0-1-1" class="clr-i-outline clr-i-outline-path-2"/><path fill="#fe5b3d" d="M8.32 9.74A1 1 0 0 0 9 10a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.42L7.33 5.92a1 1 0 0 0-1.41 1.41Z" class="clr-i-outline clr-i-outline-path-3"/><path fill="#fe5b3d" d="M27.68 26.26a1 1 0 1 0-1.42 1.42l2.41 2.4a1 1 0 0 0 .71.3a1 1 0 0 0 .7-.3a1 1 0 0 0 0-1.41Z" class="clr-i-outline clr-i-outline-path-4"/><path fill="#fe5b3d" d="M6.31 18a1 1 0 0 0-1-1h-3.4a1 1 0 0 0 0 2h3.4a1 1 0 0 0 1-1" class="clr-i-outline clr-i-outline-path-5"/><path fill="#fe5b3d" d="M34.09 17h-3.4a1 1 0 1 0 0 2h3.4a1 1 0 0 0 0-2" class="clr-i-outline clr-i-outline-path-6"/><path fill="#fe5b3d" d="m8.32 26.26l-2.4 2.41a1 1 0 0 0 .7 1.71a1 1 0 0 0 .71-.3l2.41-2.4a1 1 0 1 0-1.42-1.42" class="clr-i-outline clr-i-outline-path-7"/><path fill="#fe5b3d" d="M27 10a1 1 0 0 0 .71-.29l2.4-2.41a1 1 0 0 0 0-1.41a1 1 0 0 0-1.41 0l-2.41 2.4a1 1 0 0 0 0 1.42A1 1 0 0 0 27 10" class="clr-i-outline clr-i-outline-path-8"/><path fill="#fe5b3d" d="M18.13 7.75a10.13 10.13 0 1 0 10 10.13a10.08 10.08 0 0 0-10-10.13m0 18.25a8.13 8.13 0 1 1 8-8.12a8.08 8.08 0 0 1-8 8.12" class="clr-i-outline clr-i-outline-path-9"/><path fill="none" d="M0 0h36v36H0z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><g fill="#fe5b3d"><path d="M227.89 147.89A96 96 0 1 1 108.11 28.11a96.09 96.09 0 0 0 119.78 119.78" opacity="0.2"/><path d="M233.54 142.23a8 8 0 0 0-8-2a88.08 88.08 0 0 1-109.8-109.8a8 8 0 0 0-10-10a104.84 104.84 0 0 0-52.91 37A104 104 0 0 0 136 224a103.1 103.1 0 0 0 62.52-20.88a104.84 104.84 0 0 0 37-52.91a8 8 0 0 0-1.98-7.98m-44.64 48.11A88 88 0 0 1 65.66 67.11a89 89 0 0 1 31.4-26A106 106 0 0 0 96 56a104.11 104.11 0 0 0 104 104a106 106 0 0 0 14.92-1.06a89 89 0 0 1-26.02 31.4"/></g></svg>
+            </div>
+            <a href="register.php" class="sign-up">Sign Up</a>
+            <a href="login.php" class="sign-in">Sign In</a>
+        </div>
+    </header>
+
+    <!-- Home -->
+
+    <section class="home" id="home">
+        <div class="text">
+            <h1><span>Mau rental</span> <br>mobil?</h1>
+            <p>Temukan mobil yang sesuai dengan kebutuhan anda</p>
+            <div class="app-stores">
+                <img src="img/ios.png" alt="">
+                <img src="img/play.png" alt="">
+            </div>
+        </div>
+
+        <div class="form-container">
+            <form action="">
+                <div class="input-box">
+                    <span>Location</span>
+                    <input type="search" name="" id="" placeholder="Cari">
+                </div>
+                <div class="input-box">
+                    <span>Hari Awal Pinjam</span>
+                    <input type="date" name="" id="">
+                </div>
+                <div class="input-box">
+                    <span>Hari Pengembalian</span>
+                    <input type="date" name="" id="">
+                </div>
+                <input type="submit" name="" id="" class="btn">
+            </form>
+        </div>
+    </section>
+
+    <!-- Ride -->
+    <section class="ride" id="ride">
+        <div class="heading">
+            <span>Mau Pinjam?</span>
+            <h1>Caranya sangat mudah!!</h1>
+        </div>
+        <div class="ride-container">
+            <div class="box">
+                <i class='bx bxs-map'></i>
+                <h2>Pilih Lokasi</h2>
+                <p>Pilih lokasi anda</p>
+            </div>
+
+            <div class="box">
+                <i class='bx bxs-calendar-check'></i>
+                <h2>Pilih Hari Awal</h2>
+                <p>Pilih hari awal anda meminjam</p>
+            </div>
+            
+            
+
+            <div class="box">
+                <i class='bx bxs-calendar-star'></i>
+                <h2>Sewa Mobil</h2>
+                <p>Pilih Mobil yang ingin disewa</p>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Services -->
+    <section class="services" id="services">
+        <div class="heading">
+            <span>Best Rental</span>
+            <h1>Cari Tawaran Terbaik <br> Dari Tempat Terbaik</h1>
+        </div>
+        <div class="services-container">
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/car1.jpg" alt="">
+                </div>
+                <p>2017</p>
+                <h3>2018 Honda Civic</h3>
+                <h2>Rp400000 <span>/bulan</span></h2>
+                <a href="#" class="btn">Pilih Sekarang</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/car2.jpg" alt="">
+                </div>
+                <p>2017</p>
+                <h3>2018 Honda Civic</h3>
+                <h2>Rp400000 <span>/bulan</span></h2>
+                <a href="#" class="btn">Pilih Sekarang</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/car3.jpg" alt="">
+                </div>
+                <p>2017</p>
+                <h3>2018 Honda Civic</h3>
+                <h2>Rp400000 <span>/bulan</span></h2>
+                <a href="#" class="btn">Pilih Sekarang</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/car4.jpg" alt="">
+                </div>
+                <p>2017</p>
+                <h3>2018 Honda Civic</h3>
+                <h2>Rp400000 <span>/bulan</span></h2>
+                <a href="#" class="btn">Pilih Sekarang</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/car5.jpg" alt="">
+                </div>
+                <p>2017</p>
+                <h3>2018 Honda Civic</h3>
+                <h2>Rp400000 <span>/bulan</span></h2>
+                <a href="#" class="btn">Pilih Sekarang</a>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/car6.jpg" alt="">
+                </div>
+                <p>2017</p>
+                <h3>2018 Honda Civic</h3>
+                <h2>Rp400000 <span>/bulan</span></h2>
+                <a href="#" class="btn">Pilih Sekarang</a>
+            </div>
+        </div>
+    </section>
+    <!-- Reviews -->
+     <section class="reviews" id="reviews">
+        <div class="heading">
+            <span>Reviews</span>
+            <h1>What Our Customers Say</h1>
+        </div>
+        <div class="reviews-container">
+            <div class="box">
+                <div class="rev-img">
+                    <img src="img/rev1.jpg" alt="">
+                </div>
+                <h2>Mamat</h2>
+                <div class="stars">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half' ></i>
+                </div>
+                <p>Sudah nyewa sebulan, masih aman belum ketangkap</p>
+            </div>
+
+            <div class="box">
+                <div class="rev-img">
+                    <img src="img/rev2.jpg" alt="">
+                </div>
+                <h2>Mamat</h2>
+                <div class="stars">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half' ></i>
+                </div>
+                <p>Gacor kang</p>
+            </div>
+
+            <div class="box">
+                <div class="rev-img">
+                    <img src="img/rev3.jpg" alt="">
+                </div>
+                <h2>Putri</h2>
+                <div class="stars">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star-half' ></i>
+                </div>
+                <p>Suka banget sama servicenya, nyaman dan komplit</p>
+            </div>
+        </div>
+     </section>
+     <!-- Newsletters -->
+     <section class="newsletter">
+        <h2>Subscribe to newsletter</h2>
+        <div class="box">
+            <input type="text" placeholder="Enter Your Email...">
+            <a href="#" class="btn">Subscribe</a>
+        </div>
+     </section>
+
+     <!--ScrollReveal-->
+     <script src="https://unpkg.com/scrollreveal"></script>
+    <!-- Link Js -->
+    <script src="main.js"></script>
+</body>
+</html>
